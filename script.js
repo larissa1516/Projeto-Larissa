545f7b0e8bcdf1ffe30978276cf49704c596fb60
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-03-21 09:11:50 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
  document.addEventListener('DOMContentLoaded',function(){
     const aumentafontebotao = document.getElementById('aumentar-fonte');
+    const diminuifontebotao = document.getElementById('diminuir-fonte');
 
     let tamanhoatualfonte = 1;
 
     aumentarfontebotao.addEventListener('click',function(){
         tamanhoatualfonte += 0.1;
         document.body.style.fontSize = `${tamanhoatualfonte}rem`
+   
     })
+
+    diminuifontebotao.addEventListener('click',function(){
+        tamanhoatualfonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoatualfonte}rem`
+
+    })
+
 
  })
