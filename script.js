ce187c4a6dd834b2a71ee68457aa3f20d0de5aec
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-03-28 09:26:49 -0300

    .

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
         botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 
-       })
+     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
