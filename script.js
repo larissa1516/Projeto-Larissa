928bed33b914afa923e8909ad8ce8f7609211bd0
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-03-28 08:18:41 -0300

    .

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
- document.addEventListener('DOMContentLoaded',function(){
-    const botaodeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesdeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    botaodeAcessibilidade.addEventListener('click',function({
-    botaodeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesdeAcessibilidade.classList.toggle('apresenta-lista')
-    })
-
-    const aumentafontebotao = document.getElementById('aumentar-fonte');
-    const diminuifontebotao = document.getElementById('diminuir-fonte');
-
-    let tamanhoatualfonte = 1;
-
-    aumentarfontebotao.addEventListener('click',function(){
-        tamanhoatualfonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualfonte}rem`;
-   
-    });
-
-    diminuifontebotao.addEventListener('click', function(){
-        tamanhoatualfonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualfonte}rem`;
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
- });
+    });
+
+});
